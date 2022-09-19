48b49dae8e19a303a334128337b7a028a54d727f
albrechtLuke <lukealbrecht27@gmail.com>
2022-09-19 13:40:57 +1000

    Initial Commit

---
+++ photo/photoApp.swift
@@ -0,0 +1,17 @@
+//
+//  photoApp.swift
+//  photo
+//
+//  Created by Luke Albrecht on 19/9/2022.
+//
+
+import SwiftUI
+
+@main
+struct photoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ photoUITests/photoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  photoUITestsLaunchTests.swift
+//  photoUITests
+//
+//  Created by Luke Albrecht on 19/9/2022.
+//
+
+import XCTest
+
+final class photoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
